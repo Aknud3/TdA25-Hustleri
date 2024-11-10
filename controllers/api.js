a66d7cd5a4d47010bd0a277176cfa3acbb2c6442
Aknud3 <e.wojnar@seznam.cz>
2024-11-10 14:26:13 +0100

    Update api.js

--- controllers/api.js
+++ controllers/api.js
@@ -1,7 +1,9 @@
-export const getAPI = async(req, res) => {
+const getAPI = async(req, res) => {
     try { 
         res.status(200).json({"organization": "Student Cyber Games"})
     } catch (error) {
         res.status(500).send(error)
     }
 }
+
+module.exports = {getAPI};
