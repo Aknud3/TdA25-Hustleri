33368da956255440d2654f8bf78b1131d34f1f08
Aknud3 <168775964+Aknud3@users.noreply.github.com>
2024-11-10 15:25:24 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello Tda' });
+  res.render('index', { title: 'Hello TdA' });
 });
 
 module.exports = router;
